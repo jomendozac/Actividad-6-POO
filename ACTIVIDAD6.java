a82b5557f255f8bcb374f4810e0fcc89479da5bf
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-02-20 12:51:41 -0500

    Create ACTIVIDAD6.java

---
+++ ACTIVIDAD6.java
@@ -0,0 +1,11 @@
+package Contactos;
+// Programa que permite realizar la gestión de contactos
+
+public class ACTIVIDAD6 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal Interfaz = new VentanaPrincipal ();
+        Interfaz.setVisible(true);
+        Interfaz.setLocationRelativeTo(null);
+    }
+}
